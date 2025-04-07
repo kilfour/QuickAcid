e6ea5db6814806d65e9ae42ffd9218205b7f6e4a
kilfour <kilfour@gmail.com>
2025-04-07 12:32:15 +0200

    let's not really

--- QuickAcid/FluentContextAware/SystemSpecs.cs
+++
@@ -1,9 +0,0 @@
-namespace QuickAcid.FluentContextAware;
-
-public static class SystemSpecs
-{
-    public static Bob<Acid, TContext> Define<TContext>()
-    {
-        return new Bob<Acid, TContext>(state => new QAcidResult<Acid>(state, Acid.Test));
-    }
-}
