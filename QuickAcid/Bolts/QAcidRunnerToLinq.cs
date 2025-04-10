ca29f34068d90b2699ccf65f6dc6e0d40ce38a75
kilfour <kilfour@gmail.com>
2025-04-10 08:13:31 +0200

    apparantly not

--- QuickAcid/Bolts/QAcidRunnerToLinq.cs
+++ QuickAcid/Bolts/QAcidRunnerToLinq.cs
@@ -4,38 +4,31 @@ public static class QAcidRunnerToLinq
 {
 	public static QAcidRunner<TValueTwo> Select<TValueOne, TValueTwo>(
 		this QAcidRunner<TValueOne> runner,
-		Func<TValueOne, TValueTwo> selector)
-	{
-		return
+		Func<TValueOne, TValueTwo> selector) =>
 			state =>
-			{
-				if (state.Failed)
-					return QAcidResult.None<TValueTwo>(state);
-				return QAcidResult.Some(state, selector(runner(state).Value));
-			};
-	}
+				{
+					if (state.Failed)
+						return QAcidResult.None<TValueTwo>(state);
+					return QAcidResult.Some(state, selector(runner(state).Value));
+				};
 
 	// This is the Bind function
 	public static QAcidRunner<TResult> SelectMany<TSource, TResult>(
 		this QAcidRunner<TSource> source,
-		Func<TSource, QAcidRunner<TResult>> selector)
-	{
-		return state =>
-		{
-			if (state.Failed)
-				return QAcidResult.None<TResult>(state);
-			var result = source(state);
-			if (state.Failed)
-				return QAcidResult.None<TResult>(state);
-			return selector(result.Value)(state);
-		};
-	}
+		Func<TSource, QAcidRunner<TResult>> selector) =>
+			state =>
+				{
+					if (state.Failed)
+						return QAcidResult.None<TResult>(state);
+					var result = source(state);
+					if (state.Failed)
+						return QAcidResult.None<TResult>(state);
+					return selector(result.Value)(state);
+				};
 
 	public static QAcidRunner<TValueThree> SelectMany<TValueOne, TValueTwo, TValueThree>(
 		this QAcidRunner<TValueOne> runner,
 		Func<TValueOne, QAcidRunner<TValueTwo>> selector,
 		Func<TValueOne, TValueTwo, TValueThree> projector)
-	{
-		return runner.SelectMany(x => selector(x).Select(y => projector(x, y)));
-	}
+			=> runner.SelectMany(x => selector(x).Select(y => projector(x, y)));
 }
