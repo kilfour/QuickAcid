352d91deb8be7a038769b2de8cccc71dbc241ea3
kilfour <kilfour@gmail.com>
2025-04-28 20:17:24 +0200

    test

--- QuickAcid.Tests/Linqy/ActCarefully/ExceptionsTests.cs
+++ QuickAcid.Tests/Linqy/ActCarefully/ExceptionsTests.cs
@@ -8,7 +8,7 @@ namespace QuickAcid.Tests.Linqy.ActCarefully;
 public class ExceptionsTests
 {
     [Fact]
-    public void HowToCatchA()
+    public void How_to_catch_one()
     {
         var run =
             from result in "act".ActCarefully(() => { throw new Exception(); })
@@ -18,17 +18,29 @@ public class ExceptionsTests
         var report = run.ReportIfFailed();
         Assert.Null(report);
     }
-    public class ExceptionOne : Exception { }
-    public class ExceptionTwo : Exception { }
 
     [Fact]
-    public void HowToCatchMultiple()
+    public void How_to_catch_multiple()
     {
         var run =
             from flag in "flag".Shrinkable(MGen.Bool())
             from result1 in "act1".ActCarefully(() => { if (flag) throw new Exception(); })
             from result2 in "act2".ActCarefully(() => { if (!flag) throw new Exception(); })
             from throws in "throws".Spec(() => result1.Throws<Exception>() || result2.Throws<Exception>())
+            select Acid.Test;
+
+        var report = run.ReportIfFailed();
+        Assert.Null(report);
+    }
+
+    [Fact]
+    public void How_to_catch_one_out_of_two()
+    {
+        var run =
+            from flag in "flag".Shrinkable(MGen.Bool())
+            from result1 in "act1".ActCarefully(() => { })
+            from result2 in "act2".ActCarefully(() => { if (flag) throw new Exception(); })
+            from throws in "throws".Spec(() => !flag || result2.Throws<Exception>())
             select Acid.Test;
 
         var report = run.ReportIfFailed();
