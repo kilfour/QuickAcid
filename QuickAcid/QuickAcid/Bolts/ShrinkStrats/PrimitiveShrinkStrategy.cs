49fa6b79688186559ff2ec2459532af9bad3cdd4
kilfour <kilfour@gmail.com>
2025-05-06 17:58:14 +0200

    bangs

--- QuickAcid/QuickAcid/Bolts/ShrinkStrats/PrimitiveShrinkStrategy.cs
+++ QuickAcid/QuickAcid/Bolts/ShrinkStrats/PrimitiveShrinkStrategy.cs
@@ -19,12 +19,12 @@ public class PrimitiveShrinkStrategy //: IShrinkStrategy
             { typeof(double),   new object[] { double.MinValue, -1.0, 0.0, 1.0, double.MaxValue, double.NaN, double.NegativeInfinity, double.PositiveInfinity } },
             { typeof(decimal),  new object[] { decimal.MinValue, -1m, 0m, 1m, decimal.MaxValue } },
             { typeof(char),     new object[] { '\0', 'a', 'Z', ' ', '\n', '\uFFFF' } },
-            { typeof(string),   new object[] { null, "", "a", "the quick brown fox", new string('-', 256), new string('-', 1024) } },
+            { typeof(string),   new object[] { null!, "", "a", "the quick brown fox", new string('-', 256), new string('-', 1024) } },
             { typeof(DateTime), new object[] { DateTime.MinValue, DateTime.MaxValue, DateTime.UnixEpoch, DateTime.UtcNow, DateTime.Today } },
             { typeof(DateTimeOffset), new object[] { DateTimeOffset.MinValue, DateTimeOffset.MaxValue, DateTimeOffset.UtcNow, DateTimeOffset.Now } },
             { typeof(TimeSpan), new object[] { TimeSpan.Zero, TimeSpan.FromSeconds(1), TimeSpan.FromDays(1), TimeSpan.MinValue, TimeSpan.MaxValue } },
             { typeof(Guid),     new object[] { Guid.Empty, Guid.NewGuid() } },
-            { typeof(Uri),      new object[] { null, new Uri("http://localhost"), new Uri("https://example.com/resource?query=1") } }
+            { typeof(Uri),      new object[] { null!, new Uri("http://localhost"), new Uri("https://example.com/resource?query=1") } }
         };
 
     public ShrinkOutcome Shrink<T>(QAcidState state, string key, T value, Func<object, bool> shrinkingGuard)
@@ -33,8 +33,8 @@ public class PrimitiveShrinkStrategy //: IShrinkStrategy
             ? value.GetType()
             : typeof(T);
         var primitiveKey = PrimitiveValues.Keys.FirstOrDefault(k => k.IsAssignableFrom(actualType));
-        var primitiveVals = PrimitiveValues[primitiveKey];
-        var originalFails = state.ShrinkRun(key, value);
+        var primitiveVals = PrimitiveValues[primitiveKey!];
+        var originalFails = state.ShrinkRun(key, value!);
         if (!originalFails)
             return ShrinkOutcome.Irrelevant;
         var filtered = primitiveVals.Where(val => shrinkingGuard(val)).ToArray();
@@ -56,7 +56,7 @@ public class PrimitiveShrinkStrategy //: IShrinkStrategy
                 filtered
                     .Where(x => !Equals(x, value))
                     .All(x => state.ShrinkRun(key, x));
-            return failureAlwaysOccurs ? ShrinkOutcome.Irrelevant : ShrinkOutcome.Report(QuickAcidStringify.Default()(value));
+            return failureAlwaysOccurs ? ShrinkOutcome.Irrelevant : ShrinkOutcome.Report(QuickAcidStringify.Default()(value!));
         }
 
 
