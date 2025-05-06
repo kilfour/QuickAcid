632abe91c251116741bac5c6d30cc38344934f92
kilfour <kilfour@gmail.com>
2025-05-06 02:13:20 +0200

    proj

---
+++ refresh-projects.ps1
@@ -0,0 +1,5 @@
+dotnet sln remove QuickAcid/QuickAcid.csproj
+dotnet sln add QuickAcid/QuickAcid.csproj
+
+dotnet sln remove QuickAcid.Tests/QuickAcid.Tests.csproj
+dotnet sln add QuickAcid.Tests/QuickAcid.Tests.csproj
