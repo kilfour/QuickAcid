2102d6e1b692a601f6a8840cd48c2ef8a28cd4c6
kilfour <kilfour@gmail.com>
2025-05-12 15:02:05 +0200

    doc

--- QuickAcid/QuickAcid.Tests/Spike.cs
+++ QuickAcid/QuickAcid.Tests/Spike.cs
@@ -2,6 +2,7 @@
 using QuickAcid.Bolts;
 using QuickAcid.Bolts.Nuts;
 using QuickPulse;
+using QuickPulse.Arteries;
 
 namespace QuickAcid.Tests;
 
@@ -14,9 +15,20 @@ public class Spike
             from a in "act".Act(() => { })
             select Acid.Test;
 
+        var collector = new TheCollector<int>();
         var flow =
-            from state in Pulse.Start<QAcidState>()
-            from run in Pulse.Effect(() => script(state))
-            select script;
+            from s in Pulse.Start<QAcidState>()
+            from currentExecutionNumber in Pulse.Gather(0)
+            from trace in Pulse.Trace(currentExecutionNumber.Value)
+            from run in Pulse.Effect(() => script(s))
+            from increase in Pulse.Effect(() => currentExecutionNumber.Value++)
+            select s;
+
+        var state = new QAcidState(null!);
+        var signal = Signal.From(flow).SetArtery(collector);
+        //10.Times(() => signal.Pulse(state));
+        signal.PulseUntil(() => state.CurrentContext.NeedsToStop() || collector.TheExhibit.Contains(9), state);
+        var executionNumbers = collector.TheExhibit;
+        Assert.Equal(9, executionNumbers.Last());
     }
 }
