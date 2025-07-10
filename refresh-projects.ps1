1ed578b3d0f8908338a13c79a2f2d7e4b5404c41
kilfour <kilfour@gmail.com>
2025-07-10 15:21:01 +0200

    leaner

--- refresh-projects.ps1
+++
@@ -1,3 +0,0 @@
-
-dotnet sln remove QuickAcid/QuickAcid.Tests/QuickAcid.Tests.csproj
-dotnet sln add QuickAcid/QuickAcid.Tests/QuickAcid.Tests.csproj
--- test-it.ps1
+++
@@ -1,6 +0,0 @@
-$projects = Get-ChildItem -Recurse -Filter *.csproj | Where-Object { $_.FullName -like "*Tests*.csproj" }
-
-foreach ($proj in $projects) {
-    Write-Host "`n== Running tests for $($proj.Name) =="
-    dotnet test $proj.FullName --logger "console;verbosity=detailed"
-}
