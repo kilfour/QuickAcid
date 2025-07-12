41dfe021ac42a3d3b81ceacffa0c20c9ac231213
kilfour <kilfour@gmail.com>
2025-07-12 18:45:59 +0200

    gonna need to think about this, letting it go for now

--- QuickAcid.Tests/Shrinking/CompositeGeneratorSpike.cs
+++ QuickAcid.Tests/Shrinking/CompositeGeneratorSpike.cs
@@ -32,40 +32,27 @@ public class CompositeGeneratorSpike
     public void Using_Derived()
     {
         var script =
-            from inputOne in "inputOne".Input(MGen.Constant(42))
-            from inputTwo in "inputTwo".Input(MGen.Constant(0))
+            from inputOne in "inputOne".Input(MGen.Constant(21))
+            from inputTwo in "inputTwo".Input(MGen.Constant(21))
             from composed in "composed".Derived(MGen.Constant(new Composed() { One = inputOne, Two = inputTwo }))
             from spec in "spec".Spec(() => composed.One + composed.Two != 42)
             select Acid.Test;
-        new QState(script).TestifyOnce();
-        //var report = new QState(script).ObserveOnce();
-        //Signal.Tracing<ShrinkTrace>().SetArtery(new WriteDataToFile("CompositeGeneratorSpike2.log").ClearFile()).Pulse(report.ShrinkTraces);
+        //new QState(script).TestifyOnce();
+        var report = new QState(script).ObserveOnce();
+        //Signal.Tracing<ShrinkTrace>().SetArtery(new WriteDataToFile().ClearFile()).Pulse(report.ShrinkTraces);
+        Signal.Tracing<string>().SetArtery(new WriteDataToFile().ClearFile()).Pulse(report.Entries.Select(a => a.ToString())!);
     }
 
     [Fact(Skip = "WIP")]
     public void Desired_Result()
     {
-        // var script =
-        //     from inputOne in "inputOne".Composable(MGen.Constant(42))
-        //     from inputTwo in "inputTwo".Composable(MGen.Constant(0))
-        //     from composed in "composed".Composed(MGen.Constant(new Composed() { One = inputOne, Two = inputTwo }))
-        //     from spec in "spec".Spec(() => composed.One + composed.Two != 42)
-        //     select Acid.Test;
-        // new QState(script).TestifyOnce();
-        // var report = new QState(script).ObserveOnce();
-        // Signal.Tracing<ShrinkTrace>().SetArtery(new WriteDataToFile("CompositeGeneratorSpike2.log").ClearFile()).Pulse(report.ShrinkTraces);
-        // Signal.Tracing<string>().SetArtery(new WriteDataToFile().ClearFile()).Pulse(report.Entries.Select(a => a.ToString())!);
-    }
-
-    [Fact]
-    public void Better_Result()
-    {
-
+        // MGen.For<SomeThingToGenerate>().Customize(s => s.MyProperty, MGen.Constant(42))
         var script =
             from composed in "composed".Composed(
                 from inputOne in "inputOne".Input(MGen.Constant(42))
                 from inputTwo in "inputTwo".Input(MGen.Constant(0))
                 select new Composed() { One = inputOne, Two = inputTwo })
+            from trace in "trace".Trace(() => "should be: - Input : composed = { One : 42, Two : 0 }")
             from spec in "spec".Spec(() => composed.One + composed.Two != 42)
             select Acid.Test;
 
