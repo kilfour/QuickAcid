e6445f4014452f817c9290b759fcfa0a033452b8
kilfour <kilfour@gmail.com>
2025-08-30 08:32:07 +0200

    script execute if

--- QuickAcid/Script.cs
+++ QuickAcid/Script.cs
@@ -8,11 +8,22 @@ namespace QuickAcid;
 
 public static class Script
 {
-	public static QAcidScript<T> Execute<T>(Generator<T> generator) =>
-			state => QAcidResult.Some(state, generator(state.FuzzState).Value);
+	public static QAcidScript<Acid> Execute(Action action) =>
+		state => { action(); return QAcidResult.AcidOnly(state); };
 
+	public static QAcidScript<Acid> ExecuteIf(Func<bool> predicate, Action action) =>
+		state => { if (predicate()) action(); return QAcidResult.AcidOnly(state); };
 	public static QAcidScript<T> Execute<T>(Func<T> func) =>
 		state => QAcidResult.Some(state, func());
+
+	public static QAcidScript<T> ExecuteIf<T>(Func<bool> predicate, Func<T> func) =>
+		state => predicate() ? QAcidResult.Some(state, func()) : QAcidResult.None<T>(state);
+
+	public static QAcidScript<T> Execute<T>(Generator<T> generator) =>
+		state => QAcidResult.Some(state, generator(state.FuzzState).Value);
+
+	public static QAcidScript<T> ExecuteIf<T>(Func<bool> predicate, Generator<T> generator) =>
+		state => predicate() ? QAcidResult.Some(state, generator(state.FuzzState).Value) : QAcidResult.None<T>(state);
 
 	public static QAcidScript<T> Choose<T>(params QAcidScript<T>[] scripts)
 		=> ChooseInternal(scripts);
