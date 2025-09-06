729fb4d2b9777dd501df42b3f49f64a771b7a9e0
kilfour <kilfour@gmail.com>
2025-09-06 09:55:25 +0200

    tests

---
+++ QuickAcid.Tests/Docs/Combinators/Choose/ChooseIfTests.cs
@@ -0,0 +1,32 @@
+using QuickAcid.Tests._Tools.ThePress;
+using QuickAcid.TestsDeposition._Tools.Models;
+
+namespace QuickAcid.TestsDeposition.Docs.Combinators.Choose;
+
+
+public class ChooseIfTests
+{
+    [Fact]
+
+    public void Usage()
+    {
+        var script =
+            from observer in Script.Stashed(() => new HashSet<int>())
+            from _ in Script.ChooseIf(
+                (() => true, "One".Act(() => observer.Add(1))),
+                (() => false, "Two".Act(() => observer.Add(2))),
+                (() => true, "Three".Act(() => observer.Add(3))))
+            from as1 in "Action Assert".Assay(
+                ("One Seen", () => observer.Contains(1)),
+                ("Two Not Seen", () => !observer.Contains(2)),
+                ("Three Seen", () => observer.Contains(3)))
+            select Acid.Test;
+
+        var caseFile = QState.Run("temp", script)
+            .WithOneRun()
+            .And(20.ExecutionsPerRun());
+        var article = TheJournalist.Unearths(caseFile);
+        Assert.False(article.VerdictReached());
+    }
+}
+
--- QuickAcid.Tests/Docs/Combinators/Choose/ChooseTests.cs
+++ QuickAcid.Tests/Docs/Combinators/Choose/ChooseTests.cs
@@ -22,23 +22,17 @@ from _ in ""ops"".Choose(
 ")]
     public void Choose_usage()
     {
-        // var hashset = new HashSet<string>();
-        // var script =
-        //     from account in "Account".Stashed(() => new Account())
-        //     from _ in "ops".Choose(
-        //         "deposit".Act(() => signal.pulse),
-        //         "withdraw".Act(() => account.Withdraw(500))
-        //         )
-        //     from testify in "".TestifyProvenWhen()
-        //     from spec in "fail".Spec(() => false)
-        //     select Acid.Test;
-        // var report = QState.Run(script)
-        // .Options(a => a with { DontThrow = true })
-        // .WithOneRun()
-        // .And(2);
-        // var entry = report.First<ReportExecutionEntry>();
-        // Assert.NotNull(entry);
-        // Assert.Equal("act", entry.Key);
+        var script =
+            from signal in Script.Stashed(() => new HashSet<int>())
+            from _ in Script.Choose(
+                "one".Act(() => signal.Add(1)),
+                "two".Act(() => signal.Add(2))
+                )
+            from __ in "all actions hit".TestifyProvenWhen(() => signal.Count == 2)
+            select Acid.Test;
+        QState.Run(script)
+            .WithOneRun()
+            .And(50.ExecutionsPerRun());
     }
 }
 
